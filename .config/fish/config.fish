2a783cd5f9078c6d9fc54d01ce9cc16635e40215
aldakata <ac.tatjer@gmail.com>
2025-11-23 23:08:26 +0100

    hyprlock set up + background

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+# Very buggy and hard to kill
+alias killcode 'pgrep -f "code" | xargs kill -9'
+
+alias v="nvim"
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
